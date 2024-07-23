255b10c0483a1435d79fcf37f754b4d565b005fb
Onisim <onisimignatenkov19@gmail.com>
2024-07-23 20:31:35 +0300

    second commit

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    # Поле slug устанавливается авт. с пом. поля name:
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price',
+                    'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    # Поля, которые можно редактировать (они должны быть в list_display):
+    list_editable = ['price', 'available']
+    # Поле slug устанавливается авт. с пом. поля name:
+    prepopulated_fields = {'slug': ('name',)}
--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -21,11 +21,11 @@ class Product(models.Model):
                                  related_name='products',
                                  on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)     # Слаг товара для красивых URL
     image = models.ImageField(upload_to='products/%Y/%m/%d',
                               blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # decimal_places - кол-во разрядов
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
