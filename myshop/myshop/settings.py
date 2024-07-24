1c0702ed30b8a0e443e20bee60da718d9d456635
Onisim <onisimignatenkov19@gmail.com>
2024-07-24 16:45:40 +0300

    third commit

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -109,3 +109,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
