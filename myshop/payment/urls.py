c3c34fedcc40f8dd4d7588e0ac10a91cb2628880
Onisim <onisimignatenkov19@gmail.com>
2024-08-16 21:05:58 +0300

    9

--- myshop/payment/urls.py
+++ myshop/payment/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from . import webhooks
 
 
 app_name = 'payment'
@@ -7,5 +8,6 @@ app_name = 'payment'
 urlpatterns = [
     path('process/', views.payment_process, name='process'),
     path('completed/', views.pyment_completed, name='completed'),
-    path('ccanceled/', views.pyment_canceled, name='canceled'),
+    path('canceled/', views.pyment_canceled, name='canceled'),
+    path('webhook/', webhooks.stripe_webhook, name='stripe-webhook'),
 ]
---
+++ myshop/payment/webhooks.py
@@ -0,0 +1,39 @@
+import stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from orders.models import Order
+
+
+@csrf_exempt  # <-- Предотвращает выполнение CSRF
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload,
+            sig_header,
+            settings.STRIPE_WEBHOOK_SECRET)
+    except ValueError as e:
+        # Недопустимая полезная нагрузка:
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVarificationError as e:
+        # Недопустимая подпись:
+        return HttpResponse(status=400)
+
+    if event.type == 'checkout.session.completed':   # <-- Событие успешного завершения платежа
+        session = event.data.object  # <-- Извлекается сеансовый объект
+        if session.mode == 'payment' and session.payment_status == 'paid':
+            try:
+                order = Order.objects.get(id=session.client_reference_id)
+            except Order.DoesNotExist:
+                return HttpResponse(status=400)
+            # Пометить заказ как оплаченный:
+            order.paid = True
+            # Сохранить ID платежа Stripe:
+            order.stripe_id = session.payment_intent
+            order.save()
+
+    return HttpResponse(status=200)
